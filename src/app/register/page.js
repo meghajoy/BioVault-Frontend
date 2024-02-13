5b5b3a9857193b4c59173fac44492e7a0648445e
Your NameMegha <you@example.coaamimeghajoy@gmail.comgit config --global user.email you@example.comm>
2024-02-13 13:51:08 +0530

    logincopy and register ready

--- src/app/register/page.js
+++ src/app/register/page.js
@@ -1,27 +1,38 @@
+"use client";
+
 import Image from "next/image";
 import styles from "./register.module.css";
+import Link from "next/link";
 
 export default function Register() {
   return (
     <main>
       <div className={styles.bioVault}>
-        B<span className={styles.iitalics}>i</span>oVa<span className={styles.letteru}>u</span>
+        B<span className={styles.iitalics}>i</span>oVa
+        <span className={styles.letteru}>u</span>
         <span className={styles.litalics}>l</span>
         <span className={styles.blackText}>t</span>
       </div>
       <div className={styles.circle}>
         <Image src="/u1.png" width={407} height={501} alt="Fingerprint" />
       </div>
-      <Image className={styles.finger2}src="/u2.png" width={247} height={380} alt="Fingerprint" />
+      <Image
+        className={styles.finger2}
+        src="/u2.png"
+        width={247}
+        height={380}
+        alt="Fingerprint"
+      />
       <div className={styles.roundrect}>
         <span className={styles.reg}>Register your fingerprint</span>
-        <span className={styles.regcircle}>
+        <Link className={styles.regcircle} href="/regnext">
           <Image src="/u3.png" width={35} height={45} alt="Fingerprint" />
-        </span>
+        </Link>
       </div>
-      <div className={styles.login}>Login
-        <Image src="/u4.png" width={52} height={52} alt="Fingerprint" />
-      </div>
+      <Link type="button" className={styles.login} href="/logincopy">
+        Login
+        <Image src="/u4.png" width={40} height={40} alt="Fingerprint" />
+      </Link>
     </main>
   );
 }
--- src/app/regnext/page.js
+++ src/app/regnext/page.js
@@ -1,53 +1,163 @@
+"use client";
 import Image from "next/image";
 import styles from "./regnext.module.css";
+import { useState } from "react";
+import Link from "next/link";
 
 export default function RegNext() {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [selectedFile, setSelectedFile] = useState("");
+  const [phoneNumber, setPhoneNumber] = useState("");
+
+  const handleNameChange = (e) => {
+    setName(e.target.value);
+  };
+
+  const handleEmailChange = (e) => {
+    setEmail(e.target.value);
+  };
+  const handleFileChange = (event) => {
+    const file = event.target.files[0];
+    setSelectedFile(file);
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log("Name", name);
+    console.log("Email", email);
+  };
+  const handlePhoneNumberChange = (e) => {
+    setPhoneNumber(e.target.value);
+  };
   return (
     <main>
-        <div className={styles.rect}>
-        <Image src="/rectnoise.png" width={640} height={836} alt="Fingerprint" />
+      <div className={styles.rect}>
+        <Image
+          src="/rectnoise.png"
+          width={640}
+          height={836}
+          alt="Fingerprint"
+        />
 
         <div className={styles.bioVault}>
-        B<span className={styles.iitalics}>i</span>oVa<span className={styles.letteru}>u</span>
-        <span className={styles.litalics}>l</span>
-        <span className={styles.blackText}>t</span>
+          B<span className={styles.iitalics}>i</span>oVa
+          <span className={styles.letteru}>u</span>
+          <span className={styles.litalics}>l</span>
+          <span className={styles.blackText}>t</span>
         </div>
+      </div>
+      <div className={styles.circle}>
+        <Image
+          src="/Ellipse 10.png"
+          width={350}
+          height={350}
+          alt="Fingerprint"
+        />
+      </div>
+      <Image
+        className={styles.u5finger}
+        src="/u5.png"
+        width={348}
+        height={360}
+        alt="Fingerprint"
+      />
+      <Image
+        className={styles.cufinger}
+        src="/CuntPrint.png"
+        width={40}
+        height={62}
+        alt="Fingerprint"
+      />
+      <span className={styles.register}>Register</span>
+      <div className={styles.linesContainer}>
+        <div className={styles.formGroup}>
+          <label htmlFor="name">Name</label>
+          <input
+            type="text"
+            id="name"
+            name="name"
+            value={name}
+            onChange={handleNameChange}
+            className={styles}
+          />
         </div>
-        <div className={styles.circle}>
-             <Image src="/Ellipse 10.png" width={350} height={350} alt="Fingerprint" />
-            </div>
-            <Image className={styles.u5finger}src="/u5.png" width={348} height={360} alt="Fingerprint" />
-            <Image className={styles.cufinger}src="/CuntPrint.png" width={40} height={62} alt="Fingerprint" />
-        <span className={styles.register}>Register</span>
-        <div className={styles.linesContainer}>
-            <div className={styles.line}></div>
-            <div className={styles.line}></div>
-            <div className={styles.line}></div>
+
+        <div className={styles.line}></div>
+        <div className={styles.line}></div>
+        <div className={styles.line}></div>
         <div className={styles.enter}>
-            <span className={styles.entering}>Name</span>
-            <span className={styles.entering}>Email</span>
-            <span className={styles.entering}>Phone no.</span> 
-            <span className={styles.entering}>Add your fingerprints</span> 
+          <span className={styles.entering}>Name</span>
+          <span className={styles.entering}>Email</span>
+          <span className={styles.entering}>Phone no.</span>
+          <span className={styles.entering}>Add your fingerprints</span>
+        </div>
+      </div>
+      <div className={styles.outcircle1}></div>
+      <div className={styles.outcircle2}></div>
+      <div className={styles.outcircle3}></div>
 
-        </div>
-        </div>
-        <div className={styles.outcircle1}></div>
-        <div className={styles.outcircle2}></div>
-        <div className={styles.outcircle3}></div>
+      <div className={styles.boxcontainer}>
+        <button className={styles.box}></button>
+        <button className={styles.box}></button>
+        <button className={styles.box}></button>
+      </div>
 
-        <div className={styles.boxcontainer}>
-            <div className={styles.box}></div>
-            <div className={styles.box}></div>
-            <div className={styles.box}></div>
+      <div className={styles.pluscontainer}>
+        <div>
+          {}
+          <input
+            type="file"
+            accept="image/*"
+            id="fileInput"
+            style={{ display: "none" }}
+            onChange={handleFileChange}
+          />
+
+          <label htmlFor="fileInput" style={{ cursor: "pointer" }}>
+            <Image
+              className={styles.plus}
+              src="/plus.png"
+              width={24}
+              height={24}
+              alt="Fingerprint"
+            />
+          </label>
+          <label htmlFor="fileInput" style={{ cursor: "pointer" }}>
+            <Image
+              className={styles.plus}
+              src="/plus.png"
+              width={24}
+              height={24}
+              alt="Fingerprint"
+            />
+          </label>
+          <label htmlFor="fileInput" style={{ cursor: "pointer" }}>
+            <Image
+              className={styles.plus}
+              src="/plus.png"
+              width={24}
+              height={24}
+              alt="Fingerprint"
+            />
+          </label>
+
+          {/* {selectedFile && <p>Selected File: {selectedFile.name}</p>} */}
         </div>
-        <div className={styles.pluscontainer}>
-          <Image className={styles.plus}src="/plus.png"width={24} height={24} alt="Fingerprint" />
-          <Image className={styles.plus}src="/plus.png"width={24} height={24} alt="Fingerprint" />  
-          <Image className={styles.plus}src="/plus.png"width={24} height={24} alt="Fingerprint" />
-         </div>
-        <div className={styles.submit}>Submit
-        <Image className={styles.plus}src="/submit.png"width={92} height={34} alt="Fingerprint" />
+      </div>
+
+      <button type="submit" className={styles.submit}>
+        Submit
+        <div>
+          <Image
+            className={styles.plus}
+            src="/submit.png"
+            width={92}
+            height={34}
+            alt="Fingerprint"
+          />
         </div>
+      </button>
     </main>
   );
 }
