462d958394b9508c048010fee1009f1e723a5d14
Edna Jason <84307373+EdnaJason@users.noreply.github.com>
2024-04-22 20:09:35 +0530

    Update api.js

--- api.js
+++ api.js
@@ -6,6 +6,7 @@ const api =axios.create({
   // baseURL:"https://biovault-backend.onrender.com/"
     //baseURL:'http://localhost:5000'
     //baseURL:"https://biovault-1.onrender.com"
-   baseURL:"https://biovault-backend.onrender.com/"
+ //  baseURL:"https://biovault-backend.onrender.com/"
+   baseURL:"https://biovault-g2pi.onrender.com"
 })
 export default api
